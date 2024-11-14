e69969af16435b24df5267f71e51ea8e0a850f42
Rohit Das <141236628+rohitdasweaversweb@users.noreply.github.com>
2024-11-14 12:29:30 +0530

    Delete custom-image-gallery directory

--- custom-image-gallery/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-/*
-image gallery
-*/
--- custom-image-gallery/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('WP_UNINSTALL_PLUGIN')){
-    die;
-}
-
-global $wpdb, $table_prefix;
-$wp_gallery = $table_prefix . 'image_gallery';
-$q="DROP TABLE `$wp_gallery`";
-$wpdb->query($q);
